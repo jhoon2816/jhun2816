de60d085d298c4ace7d5ea37748fdd24553b4410
Jeonghoon <jhoon2816@gmail.com>
2017-01-17 15:53:46 +0900

    주석 수정(master)

--- js/app.js
+++ js/app.js
@@ -2,11 +2,22 @@ function generate(){
 	var number = [];
 	
 	for (var i = 0; i < 6; i++){
-
 		numbers.push(Math.floor(Math.random() * 45)+1);
-
+		
+		if(contain(numbers, number)){
+			i--;
+		} else {
+		  numbers.push(number);
+		}
 	}
-
+	
 	return numbers;
 }
 
+/* 배열 안에 nubmer가 포함되어 있는지 확인 */	
+function contain(numbers, number){
+	for(var i = 0 ;i < numbers.length; i++){
+		if(number[i] == number) return true;
+	}
+	return false;
+}
