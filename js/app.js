45d8d3399bc0aae7fb4839e6f5086ea10a3b1306
Jeonghoon <jhoon2816@gmail.com>
2017-01-17 15:24:06 +0900

    버그 수정 - 숫자 생성 범위 조정

--- js/app.js
+++ js/app.js
@@ -2,7 +2,9 @@ function generate(){
 	var number = [];
 	
 	for (var i = 0; i < 6; i++){
-		numbers.push(Math.floor(Math.random() * 45));
+
+		numbers.push(Math.floor(Math.random() * 45)+1);
+
 	}
 
 	return numbers;
